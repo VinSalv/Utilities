33dcc97294471d6319b81bdc456c0ae55a66c022
VincenzoSalvati <v.salvati10@studenti.unisa.it>
2021-08-09 11:40:25 +0200

    Fragments

--- app/src/main/java/com/example/utilities/MainActivity.java
+++ app/src/main/java/com/example/utilities/MainActivity.java
@@ -7,12 +7,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import com.example.utilities.databinding.ActivityMainBinding;
+import com.example.utilities.ui.main.SampleFragmentPagerAdapter;
 import com.example.utilities.ui.main.SectionsPagerAdapter;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
+    // configure icons
+    private final int[] imageResId = {
+            R.drawable.icona_di_prova,
+            R.drawable.icona_di_prova,
+            R.drawable.icona_di_prova};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +30,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+
+        // Get the ViewPager and set it's PagerAdapter so that it can display items
         ViewPager viewPager = binding.viewPager;
-        viewPager.setAdapter(sectionsPagerAdapter);
+        viewPager.setAdapter(new SampleFragmentPagerAdapter(getSupportFragmentManager(),
+                MainActivity.this));
+        // Give the TabLayout the ViewPager
         TabLayout tabs = binding.tabs;
         tabs.setupWithViewPager(viewPager);
+        // setup TabLayout first
+        for (int i = 0; i < imageResId.length; i++) {
+            Objects.requireNonNull(tabs.getTabAt(i)).setIcon(imageResId[i]);
+        }
+
         FloatingActionButton fab = binding.fab;
 
         fab.setOnClickListener(new View.OnClickListener() {
