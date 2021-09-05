4ce83a5b818a2b64f857c87d612dfdef6bb2a046
VincenzoSalvati <v.salvati10@studenti.unisa.it>
2021-09-05 12:43:33 +0200

    fix

--- app/src/main/java/com/app/utilities/utilitiesActivity/ARMeasureActivity.java
+++ app/src/main/java/com/app/utilities/utilitiesActivity/ARMeasureActivity.java
@@ -25,6 +25,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.app.utilities.R;
+import com.app.utilities.utility.Arl_saved;
 import com.app.utilities.utility.Preferences;
 import com.app.utilities.utility.Utils;
 import com.google.ar.core.Anchor;
@@ -50,7 +51,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-@SuppressWarnings({"JavaDoc", "unused"})
+@SuppressWarnings("ALL")
 public class ARMeasureActivity extends AppCompatActivity {
 
     private static final double MIN_OPENGL_VERSION = 3.0;
@@ -86,6 +87,7 @@ public class ARMeasureActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         pref = utils.loadData(this, new Preferences());
         if (!pref.getPredBool()) {
             if (pref.getThemeText().equals("LightTheme") || pref.getThemeText().equals("LightThemeSelected"))
@@ -103,28 +105,29 @@ public class ARMeasureActivity extends AppCompatActivity {
                     break;
             }
         }
-        if (!checkIsSupportedDeviceOrFinish(this)) {
-            return;
-        }
-        try {
-            Objects.requireNonNull(this.getSupportActionBar()).hide();
-        } catch (NullPointerException e) {
-            e.printStackTrace();
-        }
-        isARCoreSupportedAndUpToDate();
-        try {
-            createSession();
-        } catch (UnavailableSdkTooOldException e) {
-            e.printStackTrace();
-        } catch (UnavailableDeviceNotCompatibleException e) {
-            e.printStackTrace();
-        } catch (UnavailableArcoreNotInstalledException e) {
-            e.printStackTrace();
-        } catch (UnavailableApkTooOldException e) {
-            e.printStackTrace();
-        }
+
+        if (isARCoreSupportedAndUpToDate(this))
+            try {
+                createSession();
+                Objects.requireNonNull(this.getSupportActionBar()).hide();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        else utils.goToMainActivity(this);
+
         setContentView(R.layout.activity_armeasure);
+
+        try {
+            ArrayList<Arl_saved> arl_saved_object = getIntent().getExtras().getParcelableArrayList("arl_saved_object");
+            for (Arl_saved s : arl_saved_object) {
+                arl_saved.add(s.getArl_saved());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
+
         text = findViewById(R.id.text);
 
         sk_height_control = findViewById(R.id.sk_height_control);
@@ -138,13 +141,13 @@ public class ARMeasureActivity extends AppCompatActivity {
         btn_width.setOnClickListener(view -> {
             resetLayout();
             measure_height = false;
-            text.setText("Clicca sugli estremi che vuoi misurare");
+            text.setText("Clicca sugli estremi che vuoi misurare\n(sui puntini bianchi)");
         });
 
         btn_height.setOnClickListener(view -> {
             resetLayout();
             measure_height = true;
-            text.setText("Fare clic sulla base dell'oggetto che si vuole misurare");
+            text.setText("Fare clic sulla base dell'oggetto che si vuole misurare\n(sui puntini bianchi)");
         });
 
         btn_save.setOnClickListener(view -> {
@@ -247,17 +250,37 @@ public class ARMeasureActivity extends AppCompatActivity {
                 });
         ImageButton question = findViewById(R.id.question);
         question.setOnClickListener(view -> {
-            Toast toast = Toast.makeText(this, "1) Ispeziona l'area con il telecamera finchè la manina non scompare;\n\n2) Scegli quale misura adoperare;\n\n3) Piazza i cubi sui puntini bianchi che appaiono dopo aver selezionato la misura;\n\n4) Salva tutte le misure che desideri cliccando sul pulsante \"SALVA PER CONDIVIDERE\";\n\n5) Clicca sull'icona in alto a destra per usare le tue misure.\n\nP.S. Nel caso dell'altezza non sarà necessario il secondo cubo per delimitare l'estremità.", Toast.LENGTH_LONG);
+            Toast toast = Toast.makeText(this, "1) Ispeziona l'area con la telecamera finchè la manina non scompare;\n\n2) Scegli quale misura adoperare;\n\n3) Piazza i cubi:\n   - 2 per la larghezza;\n   - 1 per l'altezza;\n\n4) Salva tutte le misure che desideri cliccando sul pulsante \"SALVA MISURA\";\n\n5) Clicca sull'icona in alto a destra per usare le tue misure.\n\nP.S. Ogni quando si cambia piano di misurazione si raccomanda di cliccare sul pulsante refresh in alto (non si perderanno le misure già acquisite).\n\nPer cancellare le misure acquisite fino ad ora è necessario uscire e rientrare dall'attività \"Metro\".\n\nPuoi acquisire/salvare una sola misura per volta.", Toast.LENGTH_LONG);
             toast.setGravity(Gravity.CENTER, 0, 0);
             toast.show();
         });
 
+        ImageButton refresh = findViewById(R.id.refresh);
+        refresh.setOnClickListener(view -> {
+            Intent intent = new Intent(this, ARMeasureActivity.class);
+            ArrayList<Arl_saved> arl_saved_obj = new ArrayList<Arl_saved>();
+            for (String s : arl_saved) {
+                arl_saved_obj.add(new Arl_saved(s));
+            }
+            intent.putExtra("arl_saved_object", arl_saved_obj);
+            finish();
+            startActivity(intent);
+        });
+
         ImageButton back = findViewById(R.id.back);
         back.setOnClickListener(view -> onBackPressed());
+
         mPrevConfig = new Configuration(getResources().getConfiguration());
     }
 
-    private boolean isARCoreSupportedAndUpToDate() {
+    private boolean isARCoreSupportedAndUpToDate(final Activity activity) {
+        if (Build.VERSION.SDK_INT < VERSION_CODES.N) {
+            Toast.makeText(activity, "Sceneform richiede Android N o successivo", Toast.LENGTH_LONG).show();
+        }
+        String openGlVersionString = ((ActivityManager) activity.getSystemService(Context.ACTIVITY_SERVICE)).getDeviceConfigurationInfo().getGlEsVersion();
+        if (Double.parseDouble(openGlVersionString) < MIN_OPENGL_VERSION) {
+            Toast.makeText(activity, "Sceneform richiede OpenGL ES 3.0 o successivo", Toast.LENGTH_LONG).show();
+        }
         ArCoreApk.Availability availability = ArCoreApk.getInstance().checkAvailability(this);
         switch (availability) {
             case SUPPORTED_INSTALLED:
@@ -269,7 +292,6 @@ public class ARMeasureActivity extends AppCompatActivity {
                     switch (installStatus) {
                         case INSTALL_REQUESTED:
                             utils.notifyUser(this, "Si necessita di scaricare/aggiornare il servizio AR di Google dallo store");
-                            utils.goToMainActivity(this);
                             return false;
                         case INSTALLED:
                             return true;
@@ -280,24 +302,19 @@ public class ARMeasureActivity extends AppCompatActivity {
                 return false;
             case UNSUPPORTED_DEVICE_NOT_CAPABLE:
                 utils.notifyUser(this, "Device non supportato per la tecnologia AR Camera");
-                utils.goToMainActivity(this);
                 return false;
             case UNKNOWN_CHECKING:
             case UNKNOWN_ERROR:
                 // ARCore is checking the availability with a remote query.
                 // This function should be called again after waiting 200 ms to determine the query result.
-                utils.goToMainActivity(this);
                 utils.notifyUser(this, "Si è verificato un errore sconosciuto");
                 return false;
             case UNKNOWN_TIMED_OUT:
                 // There was an error checking for AR availability. This may be due to the device being offline.
                 // Handle the error appropriately.
-                utils.goToMainActivity(this);
                 utils.notifyUser(this, "Non è stato possibile verificare se il device supporta la tecnologia AR Camera.\nRiprova.");
                 return false;
         }
-        utils.goToMainActivity(this);
-        utils.notifyUser(this, "Si è verificato un errore sconosciuto");
         return false;
     }
 
@@ -318,6 +335,7 @@ public class ARMeasureActivity extends AppCompatActivity {
      * @param an anchor belonging to the object that should be raised
      * @param up distance in centimeters the object should be raised vertically
      */
+    @SuppressWarnings("unused")
     private void ascend(AnchorNode an, float up) {
         Anchor anchor = myhit.getTrackable().createAnchor(
                 myhit.getHitPose().compose(Pose.makeTranslation(0, up / 100f, 0)));
@@ -341,38 +359,11 @@ public class ARMeasureActivity extends AppCompatActivity {
         return (float) Math.sqrt(totalDistanceSquared);
     }
 
-    /**
-     * Check whether the device supports the tools required to use the measurement tools
-     *
-     * @param activity
-     * @return boolean determining whether the device is supported or not
-     */
-    private boolean checkIsSupportedDeviceOrFinish(final Activity activity) {
-        if (Build.VERSION.SDK_INT < VERSION_CODES.N) {
-            Toast.makeText(activity, "Sceneform richiede Android N o successivo", Toast.LENGTH_LONG).show();
-            activity.finish();
-            return false;
-        }
-        String openGlVersionString =
-                ((ActivityManager) activity.getSystemService(Context.ACTIVITY_SERVICE))
-                        .getDeviceConfigurationInfo()
-                        .getGlEsVersion();
-        if (Double.parseDouble(openGlVersionString) < MIN_OPENGL_VERSION) {
-            Toast.makeText(activity, "Sceneform richiede OpenGL ES 3.0 o successivo", Toast.LENGTH_LONG)
-                    .show();
-            activity.finish();
-            return false;
-        }
-        return true;
-    }
-
     private void saveDialog() {
         AlertDialog.Builder mBuilder = new AlertDialog.Builder(ARMeasureActivity.this);
         View mView = getLayoutInflater().inflate(R.layout.dialog_save, null);
-
         EditText et_measure = mView.findViewById(R.id.et_measure);
         mBuilder.setTitle("Titolo della misura");
-
         mBuilder.setPositiveButton("Ok", (dialogInterface, i) -> {
             if (et_measure.length() != 0) {
                 arl_saved.add(et_measure.getText() + ": " + form_numbers.format(fl_measurement));
@@ -380,10 +371,8 @@ public class ARMeasureActivity extends AppCompatActivity {
             } else
                 Toast.makeText(ARMeasureActivity.this, "Il titolo non può essere vuoto", Toast.LENGTH_SHORT).show();
         });
-
         mBuilder.setView(mView);
         AlertDialog dialog = mBuilder.create();
-
         dialog.show();
     }
 
