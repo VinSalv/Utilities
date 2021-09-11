9be7b1f5a3656c8d139766d9b49dc6aa57a82bd7
VincenzoSalvati <v.salvati10@studenti.unisa.it>
2021-09-11 15:40:21 +0200

    fix

--- app/build/generated/source/buildConfig/debug/com/app/utilities/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/app/utilities/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "com.app.utilities";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 24;
-  public static final String VERSION_NAME = "24.0";
+  public static final int VERSION_CODE = 25;
+  public static final String VERSION_NAME = "25.0";
 }
